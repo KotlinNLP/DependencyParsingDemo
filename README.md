607d8b04df33a4fec2f620ab511e377b96059e90
KotlinNLP Authors <github@kotlinnlp.com>
2017-12-15 20:33:02 +0100

    Add initial files

---
+++ README.md
@@ -0,0 +1,23 @@
+# DependencyParsingDemo
+
+DependencyParsingDemo is a web page that shows the results of [KotlinNLP](https://github.com/KotlinNLP "KotlinNLP on GitHub") 
+modules: 
+[NeuralParser](https://github.com/KotlinNLP/NeuralParser "NeuralParser on GitHub"), 
+[NeuralTokenizer](https://github.com/KotlinNLP/NeuralTokenizer "NeuralTokenizer on GitHub") and 
+[LanguageDetector](https://github.com/KotlinNLP/LanguageDetector "LanguageDetector on GitHub"). 
+
+Morpho-syntactic information are obtained calling the 
+[DependencyTreeServer](https://github.com/KotlinNLP/DependencyTreeServer "DependencyTreeServer on GitHub") backend and
+they are represented drawing dependency trees using the 
+[DependencyTreeJS](https://github.com/KotlinNLP/DependencyTreeJS "DependencyTreeJS on GitHub") library.
+
+## License
+
+This software is released under the terms of the 
+[Mozilla Public License, v. 2.0](https://mozilla.org/MPL/2.0/ "Mozilla Public License, v. 2.0")
+
+
+## Contributions
+
+We greatly appreciate any bug reports and contributions, which can be made by filing an issue or making a pull 
+request through the [github page](https://github.com/KotlinNLP/DependencyParsingDemo "DependencyParsingDemo on GitHub").
---
+++ js/main.js
@@ -0,0 +1,307 @@
+
+(function($) {
+
+    var DEBUG = true, // save last parsed text in cookies and log the response
+        KEYPRESS_MIN_INTERVAL = 300, // in milliseconds
+        isLoading = false,
+        keypressTimeout = null,
+        normalColor = "#A3ADC7",
+        hoverColor = "#FF9C2F",
+        clickColor = "#5496DC",
+        DT = null;
+    
+    $("#text-input").on("input paste", startTimeout);
+    $("#chk-realtime").change(disableTimeout);
+    $("#parse-btn").click(onParseButtonClick);
+
+    $(document).ready(onReady);
+
+    // --------------------------------------------
+
+    function onReady() {
+
+        initDT();
+
+        $("#text-input").focus();
+
+        if (DEBUG) {
+            restoreText();
+            parseText();
+        }
+    }
+
+    function initDT() {
+
+        var options = {
+                "styles": {
+                    "atom": {
+                        "rect": {
+                            "normal": {
+                                "stroke": normalColor
+                            },
+                            "hover": {
+                                "stroke": hoverColor,
+                                "stroke-width": 2,
+                                "fill": ""
+                            },
+                            "click": {
+                                "stroke": clickColor,
+                                "stroke-width": 2,
+                                "fill": ""
+                            }
+                        },
+                        "form": {
+                            "normal": {
+                                "fill": "#EAEAEA",
+                                "font": "1em proxima-nova"
+                            },
+                            "hover": {
+                                "fill": "#EAEAEA",
+                                "font": "1em proxima-nova"
+                            },
+                            "click": {
+                                "fill": "#EAEAEA",
+                                "font": "1em proxima-nova"
+                            }
+                        },
+                        "pos": {
+                            "spacing": 5,
+                            "normal": {
+                                "fill": "#A09DE9",
+                                "font-weight": "bold",
+                                "font": "0.9em proxima-nova"
+                            },
+                            "hover": {
+                                "fill": hoverColor,
+                                "font-weight": "bold",
+                                "font": "0.9em proxima-nova"
+                            },
+                            "click": {
+                                "fill": clickColor,
+                                "font-weight": "bold",
+                                "font": "0.9em proxima-nova"
+                            }
+                        }
+                    },
+                    "deprel": {
+                        "normal": {
+                            "fill": normalColor,
+                            "font": "1em proxima-nova",
+                            "font-weight": "bold"
+                        },
+                        "hover": {
+                            "fill": hoverColor,
+                            "font": "1em proxima-nova",
+                            "font-weight": "bold"
+                        },
+                        "click": {
+                            "fill": clickColor,
+                            "font": "1em proxima-nova",
+                            "font-weight": "bold"
+                        },
+                        "path": {
+                            "spacing": 14,
+                            "normal": {
+                                "stroke": normalColor
+                            },
+                            "hover": {
+                                "stroke": hoverColor
+                            },
+                            "click": {
+                                "stroke": clickColor
+                            }
+                        },
+                        "padding": {
+                          "horizontal": 15
+                        },
+                        "spacing": 5
+                    }
+                }
+            };
+    
+        DT = $("#dependency-tree").dependencyTree(options);
+    }
+
+    function startTimeout() {
+
+        if ($("#chk-realtime").is(':checked')) {
+
+            disableTimeout();
+
+            if (!isLoading) {
+                keypressTimeout = setTimeout(parseText, KEYPRESS_MIN_INTERVAL)
+            }
+        }
+    }
+
+    function onParseButtonClick() {
+
+        disableTimeout();
+
+        if (!isLoading) {
+            parseText();
+        }
+    }
+
+    function disableTimeout() {
+
+        if (keypressTimeout != null) {
+            clearTimeout(keypressTimeout);
+        }        
+    }
+
+    function parseText() {
+
+        startLoading();
+
+        var text = $("#text-input").val();
+
+        if (DEBUG) {
+            saveText(text);
+        }
+    	
+        if ($.trim(text) === "") {
+
+            clearResults();
+
+            endLoading();
+
+        } else {
+
+            $.ajax({
+                "url": "http://localhost:30000/parse",
+                "method": "POST",
+                "data": {
+                    "text": text
+                },
+                "dataType": "json",
+                "success": onSuccess,
+                "error": onError,
+                "complete": endLoading
+            });
+        }
+    }
+
+    function onSuccess(data) {
+
+        if ("error" in data) {
+                
+            clearResults();
+
+            if (data.error.type == "NOT_SUPPORTED_LANGUAGE") {
+                
+                notSupportedLanguage(data.error.data.lang);
+
+            } else {
+                console.error("NLP Server error: " + data.error.type);
+            }
+            
+        } else {
+
+            if (DEBUG) {
+                console.log(data);
+            }
+
+            DT.draw(data['sentences']);
+            setLanguage(data['lang']);
+        }
+    }
+
+    function onError(jqXHR, textStatus, errorThrown) {
+        
+        console.error("NLP Server error: [" + jqXHR.status + "] " + jqXHR.statusText);
+
+        clearResults();
+    }
+
+    function notSupportedLanguage(isoCode) {
+
+        $("#language-not-supported").show();
+
+        setLanguage(isoCode, supported=false);
+    }
+
+    function clearResults() {
+
+        DT.clear();
+        
+        $("#language").hide();
+        
+    }
+
+    function startLoading() {
+
+        isLoading = true;
+
+        $("#parse-btn").attr("disabled", "disabled");
+        $("#parse-btn-label").hide();
+        $("#language-not-supported").hide();
+        $("#loading-img").show();
+    }
+
+    function endLoading() {
+
+        isLoading = false;
+
+        $("#parse-btn").removeAttr("disabled");
+        $("#loading-img").hide();
+        $("#parse-btn-label").show();
+    }
+
+    function setLanguage(isoCode, supported=true) {
+
+        if (supported) {
+            $("#language").removeClass("not-supported");
+        } else {
+            $("#language").addClass("not-supported");
+        }
+        
+        $("#language").show();
+        $("#lang-code").text(isoCode.toUpperCase());
+    }
+
+    function saveText(text) {
+        setCookie("last-input-text", text);
+    }
+
+    function restoreText() {
+
+        var prevText = getCookie("last-input-text");
+
+        if (prevText != "" && !isLoading) {
+            $("#text-input").val(prevText);
+        }
+    }
+
+    function setCookie(cname, cvalue, exdays=1) {
+
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+
+        var expires = "expires="+ d.toUTCString();
+
+        document.cookie = cname + "=" + encodeURI(cvalue) + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+
+            if (c.indexOf(name) == 0) {
+                return decodeURI(c.substring(name.length, c.length));
+            }
+        }
+
+        return "";
+    }
+
+})(jQuery);
