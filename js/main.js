c93605e29200440a4b718fc199060045210a90a1
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-17 14:59:08 +0200

    Fix join method call

--- js/main.js
+++ js/main.js
@@ -142,7 +142,7 @@
             "head": atom['syn']['head'],
             "form": atom['surface']['form'] || "Ø",
             "pos": atom['syn']['edges'][0]['pos'],
-            "deprel": atom['syn']['edges'][0]['deprel'] + (atom['sem'].length > 0 ? "-" + "/".join(atom['sem']) : ""),
+            "deprel": atom['syn']['edges'][0]['deprel'] + (atom['sem'].length > 0 ? "-" + atom['sem'].join("/") : ""),
             "sem": atom['sem'].length > 0 ? atom['sem'] : null,
             "corefs": atom['corefs'].length > 0 ? atom['corefs'].map(convertNLSCoref) : null
         }
